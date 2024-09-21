e4c3202ac8bfa8f6011dfa65e8e9ff66468b2136
Throy <genesistroy.fdc@gmail.com>
2024-09-21 18:10:01 +0800

    Normalize line endings

---
+++ entities/Controller/Admin/AdminController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Entities\Controller\Admin;
+
+use Core\Controller\Admin\AppController;
+use Illuminate\Http\Request;
+
+class AdminController extends AppController
+{
+    public function index()
+    {
+        // Display a listing of the resource
+    }
+
+    public function create()
+    {
+        // Show the form for creating a new resource
+    }
+
+    public function store(Request $request)
+    {
+        // Store a newly created resource in storage
+    }
+
+    public function show($id)
+    {
+        // Display the specified resource
+    }
+
+    public function edit($id)
+    {
+        // Show the form for editing the specified resource
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Update the specified resource in storage
+    }
+
+    public function destroy($id)
+    {
+        // Remove the specified resource from storage
+    }
+}
---
+++ entities/Model/Admin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Entities\Model;
+
+use Core\Model\AppModel;
+
+class Admin extends AppModel
+{
+    // Add your additional properties and methods here
+}
