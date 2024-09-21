f75c89e1fe6195c4873d3810ced97f100900fed5
Throy <genesistroy.fdc@gmail.com>
2024-09-21 17:20:20 +0800

    Fix All

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\UserController;
+use Entities\Controller\Admin\AdminController;
+use Entities\Controller\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
