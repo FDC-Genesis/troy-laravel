e1b27e496330aa3dad290bafde718d9b5b574908
Throy <genesistroy.fdc@gmail.com>
2024-09-21 17:20:43 +0800

    Fix All 2

---
+++ entities/Model/Admin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Entities\Admin\Model;
+
+use Core\Model\AppModel;
+
+class Admin extends AppModel
+{
+    // Add your additional properties and methods here
+}
---
+++ entities/Model/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Entities\User\Model;
+
+use Core\Model\AppModel;
+
+class User extends AppModel
+{
+    // Add your additional properties and methods here
+}
